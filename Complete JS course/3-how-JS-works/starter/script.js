73cbd29f4b8312a57c3fa02370fd2108d83e4633
minkolee <minkolee@gmail.com>
2018-11-29 19:05:18 +0800

    自行编写完第四部分的游戏

--- Complete JS course/3-how-JS-works/starter/script.js
+++ Complete JS course/3-how-JS-works/starter/script.js
@@ -1,20 +1,31 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
+// calculateAge(1990);
 
 
+// function calculateAge(year) {
+//     console.log(2016 - year);
+// }
+//
+// retirement(1990);
 
 
+// 函数表达式无法提升，只有函数声明才能提升
+// var retirement = function(year){
+//     console.log(65-(2016-calculateAge(year)));
+// };
 
+// console.log(age);
+// var age = 23;//如果不注释这行，显示undefined，如果注释掉，则会报错。因为没有了提升。
 
-
-
-
-
-
-
-
-
-
+// function foo() {
+//     console.log(age);
+//     var age = 65;
+//     console.log(age);
+// }
+//
+// foo();
+// console.log(age);
 
 
 ///////////////////////////////////////
@@ -37,7 +48,6 @@ function first() {
     }
 }
 */
-
 
 
 // Example to show the differece between execution stack and scope chain
@@ -63,15 +73,43 @@ function third() {
 */
 
 
-
 ///////////////////////////////////////
 // Lecture: The this keyword
 
+// console.log(this);
+//
+// var john = {
+//     name:"John",
+//     yearofBirth:1990,
+//     calculateAge:function () {
+//         console.log(this);
+//         function inner() {
+//             console.log("i m inner");
+//             console.log(this);
+//         }
+//         inner();//并不是绑定的函数，默认的this对象依然是window
+//     }
+// };
+//
+// john.calculateAge();
 
-
-
-
-
-
-
+// var mike = {
+//     name:"Mike",
+//     yearofBirth:1990,
+// };
+//
+// function showName() {
+//     if(this.name){
+//         console.log(this.name);
+//     } else {
+//         console.log("找不到name")
+//     }
+// }
+//
+// showName();
+//
+// mike.showName = showName;
+//
+// mike.showName();
+//绑定的方法this显然就是当前对象了。
 
--- Complete JS course/4-DOM-pig-game/starter/app.js
+++ Complete JS course/4-DOM-pig-game/starter/app.js
@@ -1,10 +1,103 @@
-/*
-GAME RULES:
+let scores, roundScore, activePlayer, dice;
+let player0Panel = document.querySelector('.player-0-panel');
+let player1Panel = document.querySelector('.player-1-panel');
+let player0Score = document.getElementById("score-0");
+let player1Score = document.getElementById("score-1");
+let newGame = document.querySelector(".btn-new");
+let rollDice = document.querySelector(".btn-roll");
+let holdDice = document.querySelector(".btn-hold");
+let dicePic = document.querySelector("img");
+let player0CurrentScore = document.getElementById("current-0");
+let player1CurrentScore = document.getElementById("current-1");
+let player0 = document.getElementById("name-0");
+let player1 = document.getElementById("name-1");
 
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
 
-*/
+function initialize() {
+    scores = [0, 0];
+    roundScore = [0, 0];
+    activePlayer = 0;
+    player0Panel.classList.add("active");
+    player1Panel.classList.remove("active");
+    player0Score.textContent = "0";
+    player1Score.textContent = "0";
+    player0CurrentScore.textContent="0";
+    player1CurrentScore.textContent="0";
+    player0.textContent="Player 1";
+    player1.textContent="Player 2";
+    rollDice.removeEventListener('click', rollHandler);
+    holdDice.removeEventListener('click',holdHandler);
+    rollDice.addEventListener('click', rollHandler);
+    holdDice.addEventListener('click',holdHandler);
+}
+
+function rollHandler() {
+    if (activePlayer === 0) {
+        let newDice = Math.floor(Math.random() * 6) + 1;
+        dicePic.src = "dice-" + newDice + ".png";
+        let currentTotalScore = parseInt(player0CurrentScore.textContent);
+        if (newDice !== 1) {
+            currentTotalScore = currentTotalScore + newDice;
+            player0CurrentScore.textContent = currentTotalScore.toString();
+        } else {
+            player0CurrentScore.textContent = "0";
+        }
+
+    }
+    if (activePlayer === 1) {
+        let newDice = Math.floor(Math.random() * 6) + 1;
+        dicePic.src = "dice-" + newDice + ".png";
+        let currentTotalScore = parseInt(player1CurrentScore.textContent);
+        if (newDice !== 1) {
+            currentTotalScore = currentTotalScore + newDice;
+            player1CurrentScore.textContent = currentTotalScore.toString();
+        } else {
+            player1CurrentScore.textContent = "0";
+        }
+
+    }
+}
+
+function holdHandler() {
+    if(activePlayer===0){
+        let currentTotalScore = parseInt(player0CurrentScore.textContent);
+        console.log(currentTotalScore);
+        scores[activePlayer] = scores[activePlayer] +=currentTotalScore;
+        player0Score.textContent = scores[activePlayer];
+        player0CurrentScore.textContent="0";
+        if(isDefinite(scores)){
+           return;
+        }
+        player0Panel.classList.remove("active");
+        player1Panel.classList.add("active");
+        activePlayer = 1;
+    }
+
+    else{
+        let currentTotalScore = parseInt(player1CurrentScore.textContent);
+        scores[activePlayer] = scores[activePlayer] +=currentTotalScore;
+        player1Score.textContent = scores[activePlayer];
+        player1CurrentScore.textContent="0";
+        if(isDefinite(scores)){
+            return;
+        }
+        activePlayer = 0;
+        player1Panel.classList.remove("active");
+        player0Panel.classList.add("active");
+    }
+}
+
+function isDefinite(scores) {
+    if(scores[activePlayer]>=100){
+        let id = "name-"+activePlayer;
+        document.getElementById(id).textContent = "WINNER";
+        rollDice.removeEventListener('click', rollHandler);
+        holdDice.removeEventListener('click',holdHandler);
+        return true;
+    }
+    return false;
+}
+
+initialize();
+newGame.addEventListener('click', initialize);
+
