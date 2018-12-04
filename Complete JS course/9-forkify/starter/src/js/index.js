499c4e18940caffee61d3bc976ce0c7f64ae5304
minkolee <minkolee@gmail.com>
2018-12-04 10:05:47 +0800

    配置好前端开发环境

--- Complete JS course/9-forkify/starter/src/js/index.js
+++ Complete JS course/9-forkify/starter/src/js/index.js
@@ -1,4 +1,6 @@
 import num from './test.js'
 
-console.log(`I imported ${num} from another test.js`);
+const x = 23;
+
+console.log(`I imported ${num} from another test.js. Variable x = ${x}`);
 console.log("修改了一些内容");
--- Complete JS course/9-forkify/starter/webpack.config.js
+++ Complete JS course/9-forkify/starter/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: './src/js/index.js',
+    entry: ["babel-polyfill",'./src/js/index.js'],
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: "js/bundle.js",
@@ -15,5 +15,29 @@ module.exports = {
             filename: 'index.html',
             template: './src/index.html'
         })
-    ]
+    ],
+    module:{
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+
+                use: {
+                    loader:'babel-loader'
+                }
+            }
+        ]
+    }
 };
+
+// 第一步:安装3个包
+
+//第二步,在webpack中配置modules-->rules
+
+//第三步,编写.babelrc文件
+
+//第四步,安装babel-polyfill,在本文件中设置新入口
+
+//如果是babel6,必须安装babel-loader@7
+
+//后边还需要学习SASS来编译CSS文件
