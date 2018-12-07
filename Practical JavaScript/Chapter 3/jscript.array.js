09fd797e1bf9dd901e3302ea18d27367d4346cfd
minkolee <minkolee@gmail.com>
2018-12-07 16:56:25 +0800

    CSS MM4 第三章

--- Practical JavaScript/Chapter 3/jscript.array.js
+++ Practical JavaScript/Chapter 3/jscript.array.js
@@ -1,12 +1,12 @@
 if (typeof jscript === "undefined") {
     jscript = function () {
-
     };
 }
 
 jscript.array = function () {
 
 };
+
 //将源数组内所有数据追加复制到目标数组中
 jscript.array.copyArray = function (inSrcArray, inDestArray) {
     var i;
@@ -14,22 +14,21 @@ jscript.array.copyArray = function (inSrcArray, inDestArray) {
         inDestArray.push(inSrcArray[i]);
     }
     return inDestArray;
-}
+};
 
 //在目标数组中查找第一个给定值的索引,找不到就返回-1
 jscript.array.findInArray = function (inArray, inValue) {
     var i;
-    for(i=0;i<inArray.length;i++){
-        if(inArray[i]===inValue);
-        return i;
+    for (i = 0; i < inArray.length; i++) {
+        if (inArray[i] === inValue) return i;
     }
     return -1;
-}
+};
 
 //计算数组元素的平均数
 jscript.array.arrayAverage = function (inArray) {
     var i;
-    var accumulator = 0
+    var accumulator = 0;
     for(i=0;i<inArray.length;i++){
         accumulator += inArray[i];
     }
