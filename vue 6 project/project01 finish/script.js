3ebb36b6c7a448cd77cef4a00e8b0a9d49d7a0dd
minkolee <minkolee@gmail.com>
2019-06-20 17:04:47 +0800

    开始看Vue项目

---
+++ vue 6 project/project01 finish/script.js
@@ -0,0 +1,43 @@
+new Vue({
+    el: '#notebook',
+    data: function () {
+        return {
+            content: localStorage.getItem('content'),
+            notes: []
+        }
+    },
+    computed: {
+        notePreview() {
+            return marked(this.content);
+        }
+    },
+    watch: {
+        content: {
+            //handler如果要调用其他方法，要用字符串形式
+            handler: 'saveNote',
+            //设置了immediate之后，这个方法会在改变前调用
+        }
+    },
+    methods: {
+        saveNote(val) {
+            console.log('saving note', val);
+            localStorage.setItem('content', val)
+        },
+        //每次调用该方法，向数组内加入一个默认note对象
+        addNote() {
+            //当前时间
+            const time = Date.now();
+            //默认note对象
+            const note = {
+                id: String(time),
+                title: 'New note ' + (this.notes.length + 1),
+                content: '**Hi!** This notebook is using [markdown](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet) for formatting!',
+                created: time,
+                favorite: false,
+            }
+            this.notes.push(note)
+        }
+    },
+})
+
+
