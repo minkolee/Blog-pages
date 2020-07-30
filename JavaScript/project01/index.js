ec6f7b5bd1a629766c609c37861334e93d17314c
minkolee <minkolee@gmail.com>
2020-07-30 17:27:22 +0800

    项目回家继续看

--- JavaScript/project01/index.js
+++ JavaScript/project01/index.js
@@ -4,13 +4,27 @@
 
 let apiKey = '3453dad4'
 
-const fetchData = async () =>{
-    const response = await axios.get("http://www.omdbapi.com/",{
-        params:{
+const fetchData = async (name) => {
+    const response = await axios.get("http://www.omdbapi.com/", {
+        params: {
             apiKey: '3453dad4',
-            i: 'tt6048922'
+            s: name
         }
     });
     console.log(response.data);
 }
 
+const input = document.querySelector('input');
+
+//避免重复点击的套路, 1.5秒钟没有输入再发送
+let timeoutId;
+const onInput = event => {
+    if (timeoutId) {
+        clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+        fetchData(event.target.value);
+    }, 1500);
+}
+
+input.addEventListener('input', onInput);
